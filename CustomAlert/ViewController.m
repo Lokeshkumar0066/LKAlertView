9bcc2bd4167dde7b5210b46049ae93be0c50a05a
Lokesh Kumar <lokesh.kumar@iifl.com>
2018-08-13 14:56:59 +0530

    Set the color and font of Alertview

--- CustomAlert/ViewController.m
+++ CustomAlert/ViewController.m
@@ -30,13 +30,13 @@
 - (IBAction)Btnclick:(id)sender {
     
     DrawAlertView *objAlert = [DrawAlertView initlization];//[DrawAlertView loadFromNib:@"DrawAlertView" classToLoad:[DrawAlertView class]]; //[DrawAlertView initlization];
- /*
+
     objAlert.messageColor = [UIColor lightGrayColor];
     objAlert.titleColor = [UIColor darkGrayColor];
     objAlert.buttonBgColor = [UIColor colorWithRed:0.0f/255.0f green:143.0f/255.0f blue:0.0f/255.0f alpha:1.0];
     objAlert.buttonTextColor = [UIColor whiteColor];
     objAlert.isEmojiFailure = NO;
-  */
+
     
     [objAlert initWithDelegate:self message:@"Learning is project created to capture time where some R&D is required for development, it is to be used for KT and Understanding new technology." title:@"Alert!!" btnOK:nil btnCancel:@"Cancel" btnDone:@"Done"];
 }
